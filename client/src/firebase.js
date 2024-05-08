d8edc05dcc6b20ece01d39e4f94bb092dabc3f37
Sonali Burman <sonali.b.020200@gmail.com>
2024-05-09 04:38:01 +0530

    edited

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,11 +3,13 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "travel-website-7dc3c.firebaseapp.com",
-  projectId: "travel-website-7dc3c",
-  storageBucket: "travel-website-7dc3c.appspot.com",
-  messagingSenderId: "503305790829",
-  appId: "1:503305790829:web:7de41302f4ff57b245a664",
+  authDomain: "travelopia-2f8a1.firebaseapp.com",
+  projectId: "travelopia-2f8a1",
+  storageBucket: "travelopia-2f8a1.appspot.com",
+  messagingSenderId: "56529896389",
+  appId: "1:56529896389:web:c4d3fc05398754702481ac",
+  measurementId: "G-D0Y11CMWMW",
+  databaseURL: "https://travelopia-2f8a1-default-rtdb.firebaseio.com/"
 };
 
 
